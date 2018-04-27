624d00b2621187cb37fc8f1005a8fb917e785275
Sebastian Hönel <development@hoenel.net>
2018-04-27 02:48:42 +0200

    Implement skeletton markup in index.html and clean up main.js, now that we have componentized everything

--- tool/script/main.js
+++ tool/script/main.js
@@ -1,35 +1,36 @@
 import * as typedefs from './typedefs.js';
 import { Dataset } from './Dataset.js';
 import { Model, Enum_Event_Types } from './Model.js';
+import { GridboxHeader, LoadEvent } from './grid/Box_Header.js';
+import { GridboxStatus } from './grid/Box_Status.js';
+import { GridboxGraph } from './grid/Box_Graph.js';
 
+/**
+ * This function does the basic stuff. However, we should write own modules
+ * for each grid-box and load them here, so it does not get too cluttered.
+ * So we stick to the basics here such as init'ing the logging etc.
+ */
 const run = async() => {
-  const pre = document.querySelector('pre');
-  const logger = val => {
-    pre.innerText = val + "\n" + pre.innerText;
-  }; // console.log;
-  logger('run');
+  const gbHeader = new GridboxHeader();
+  const gbStatus = new GridboxStatus(gbHeader.observable);
+  const gbGraph = new GridboxGraph(gbHeader.observable);
 
-  // Load our default data and QM:
-  let ds = await Dataset.fromDataAndQm(
-    '/data/default.csv', '/data/default-model.json');
+  // Instantiate more components here..
 
-  const model = new Model(ds);
-  model.print(logger);
+  gbHeader.gridboxStatus = gbStatus;
+  gbStatus.logger('run');
 
-  const start = +new Date;
-  const progress = document.querySelector('progress');
-  const obs = model.subscribe(evt => {
-    if (evt.type === Enum_Event_Types.Progress) {
-      progress.value = evt.data;
-      logger(`Progress: ${(evt.data * 100).toFixed(2)} %`);
-      if (evt.data === 1) {
-        model.unsubscribe(obs);
-      }
-    }
-  });
+  // Add more init stuff here..
+  
+  // @RAFAEL: In your components, just subscribe to the observable of the
+  // header's component (gbHeader.observable). Any event it emits will have
+  // the dataset and the model that we should use. Then you can also subs-
+  // cribe on the model or its nodes (whatever is required there).
 
-  await model.recompute();
-  logger(`Model is computed and up-2-date! Computation took ${(+new Date) - start}ms`);
+  // @RAFAEL: For any data you need access, subscribe to the model's observable
+  // to get notified of changes. Currently, there is only the progress-event,
+  // but feel free to add more in the Enum_Event_Types. But I guess that may
+  // not be necessary, because you can listen for progress with value === 1.
 };
 
 export { run };
