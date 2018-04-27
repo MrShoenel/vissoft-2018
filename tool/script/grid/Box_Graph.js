1646a0eea0d2fadb0fbaf6f317f7e8158d2d9c61
Sebastian Hönel <development@hoenel.net>
2018-04-27 21:11:41 +0200

    Add class GraphEvent and make the GridboxGraph observable for changes to the graph

--- tool/script/grid/Box_Graph.js
+++ tool/script/grid/Box_Graph.js
@@ -6,18 +6,79 @@ import { Model } from '../Model.js';
 
 
 
+class GraphEvent {
+  /**
+   * This class is propagated when the graph changed.
+   * 
+   * @template T the type of data
+   * @param {'selection'|'links'|'nodeDisabled'|'nodeEnabled'} type the type
+   * of the event that occurred
+   * @param {T} data the data associated with the event
+   */
+  constructor(type, data) {
+    this.type = type;
+    this.data = data;
+  };
+};
+
+
 class GridboxGraph {
   /**
    * @param {Rx.Observable.<LoadEvent>} dataObservable 
    */
   constructor(dataObservable) {
+    /** @type {Array.<Rx.Observer.<GraphEvent>>} */
+    this._observers = [];
+
+    this._observable = Rx.Observable.create(observer => {
+      this._observers.push(observer);
+      const oldDispose = observer.dispose;
+      observer.dispose = () => {
+        this._unsubscribe(observer);
+        oldDispose.call(observer);
+      };
+    });
+
     dataObservable.subscribe(evt => {
       this.dataset = evt.dataset;
       this.model = evt.model;
     });
   };
+
+  /**
+   * @param {LoadEvent} event the LoadEvent to emit to all observers
+   */
+  _emitEvent(event) {
+    this._observers.forEach(obs => obs.onNext(event));
+  };
+
+  /**
+   * Can be called directly but it is recommended to call dispose()
+   * on the Observer/Subscription.
+   * 
+   * @param {Rx.IDisposable|Rx.Observer.<LoadEvent>} subscriber 
+   */
+  _unsubscribe(subscriber) {
+    const idx = this._observers.findIndex(o => o === subscriber);
+    if (idx < 0) {
+      throw new Error(`The subscriber is not currently known.`);
+    }
+    this._observers.splice(idx, 1);
+  };
+
+  /**
+   * Obtain the Observable for this ModelNode that emits GraphEvent.
+   * This Observable never drains. To save resources, call dispose() on
+   * obtained Observers, when not longer needed.
+   * 
+   * @returns {Rx.Observable.<GraphEvent>}
+   */
+  get observable() {
+    return this._observable;
+  };
 };
 
 export {
+  GraphEvent,
   GridboxGraph
 };
