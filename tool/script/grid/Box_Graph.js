8b30caa8b745dd8f14a0bda22badc087578f9858
Sebastian Hönel <development@hoenel.net>
2018-04-29 20:04:42 +0200

    Implement working graph (but it's only a dummy for the moment)

--- tool/script/grid/Box_Graph.js
+++ tool/script/grid/Box_Graph.js
@@ -30,6 +30,9 @@ class GridboxGraph {
     /** @type {Array.<Rx.Observer.<GraphEvent>>} */
     this._observers = [];
 
+    /** @type {Array.<string>} the IDs pf selected entities */
+    this._selection = [];
+
     this._observable = Rx.Observable.create(observer => {
       this._observers.push(observer);
       const oldDispose = observer.dispose;
@@ -42,6 +45,10 @@ class GridboxGraph {
     dataObservable.subscribe(evt => {
       this.dataset = evt.dataset;
       this.model = evt.model;
+
+      // Initially, nothing is selected
+      this._emitEvent(new GraphEvent('selection', this._selection));
+      this._testGraph();
     });
   };
 
@@ -76,7 +83,86 @@ class GridboxGraph {
   get observable() {
     return this._observable;
   };
+
+
+
+  _testGraph() {
+    this._testJson = this._testJson || {
+      "nodes": [
+        {"name": "d3"},
+        {"name": "d3.svg"},
+        {"name": "d3.svg.area"},
+        {"name": "d3.svg.line"},
+        {"name": "d3.scale"},
+        {"name": "d3.scale.linear"},
+        {"name": "d3.scale.ordinal"}
+      ],
+      "links": [
+        {"source": 0, "target": 1},
+        {"source": 1, "target": 2},
+        {"source": 1, "target": 3},
+        {"source": 0, "target": 4},
+        {"source": 4, "target": 5},
+        {"source": 4, "target": 6}
+      ]
+    };
+
+    var width = 600,
+      height = 600,
+      radius = 6;
+
+
+    var fill = d3.scaleOrdinal(
+      ["#A07A19", "#AC30C0", "#EB9A72", "#BA86F5", "#EA22A8"]);
+
+    var mb = d3.forceManyBody().strength(-120).distanceMin(30);
+
+    var force = d3.forceSimulation(this._testJson.nodes)
+      .force('charge', mb)
+      .force('link', d3.forceLink(this._testJson.links))
+      .force('center', d3.forceCenter(width / 2, height / 2));
+
+    var svg = document.querySelector('#foo-svg') ?
+      d3.select('#foo-svg') : d3.select("#graph-div").append("svg:svg")
+      .attr('id', 'foo-svg')
+      .attr('style', 'transform:rotate(90deg)')
+      .attr("width", width)
+      .attr("height", height);
+
+    var link = svg.selectAll("line")
+      .data(this._testJson.links)
+      .enter().append("line").style('stroke', 'rgb(0,0,0');
+
+    var node = svg.selectAll("circle")
+      .data(this._testJson.nodes)
+      .enter().append("circle")
+      .attr("r", radius - .75)
+      .style("fill", function(d) { return fill(d.group); })
+      .style("stroke", function(d) { return d3.rgb(fill(d.group)).darker(); });
+
+    force.on("tick", () => {
+      var k = 6 * force.alpha();
+
+      // Push sources up and targets down to form a weak tree.
+      link
+        .each(function(d) { d.source.y -= k, d.target.y += k; })
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+      node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+    });
+  };
 };
+
+
+// class ModelToGraphConverter {
+//   constr
+// };
+
 
 export {
   GraphEvent,
